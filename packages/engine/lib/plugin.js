88a7265af51068ebe171bd7922c13a64d4210dad
Jarod Reid <jarod@solidalchemy.com>
2020-09-29 20:26:12 +0000

    working plugin loading for cli

--- packages/engine/lib/plugin.js
+++ packages/engine/lib/plugin.js
@@ -1,23 +1,40 @@
 const path = require("path");
 const glob = require("glob");
 
+const ld = require("lodash")
+
+
 class Plugin {
   constructor() {
     this.store = require("mem-fs").create();
     this.bootstrapHooks = [];
   }
 
-  bootstrap(game) {
-    glob.sync(path.join(this.root, "cli")).each((cliFile) => {
-      game.command(
-        this.name,
-        `cli commands provided by the '${name}' plugin`,
-        (cmd) => {
-          cmd.command(...require(cliFile));
-        }
-      );
-    });
+  _bootstrap(engine) {
+    this.bootstrapCli(engine)
+    this.bootstrap(engine)
   }
+
+  bootstrapCli(engine) {
+    const files = glob.sync(path.join(this.root, "cli", "**", "*.js"))
+    if (files.length > 0) {
+      engine.command(
+        ld.kebabCase(this.name),
+        `cli commands provided by the '${this.name}' plugin`,
+        (cmd) => {
+          files.forEach((cliFile) => {
+            cmd.command(...require(cliFile))
+              .showHelpOnFail(true)
+              .demandCommand()
+          })
+        }
+      )
+    }
+  }
+
+  bootstrap(engine) {
+  }
+
 }
 
 module.exports = Plugin;
